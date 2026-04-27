80336d18ad8ca2ac6cb276408225bfeb783eebff
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-27 09:21:53 -0500

    first commit

---
+++ environments/dev/output.tf
@@ -0,0 +1,9 @@
+output "bucket_name" {
+	description = "Name of the dev S3 bucket"
+	value       = module.storage.bucket_name
+}
+
+output "bucket_arn" {
+	description = "ARN of the dev S3 bucket"
+	value       = module.storage.bucket_arn
+}
---
+++ modules/storage/main.tf
@@ -0,0 +1,14 @@
+terraform {
+	required_version = ">= 1.6.0"
+
+	required_providers {
+		aws = {
+			source  = "hashicorp/aws"
+			version = "~> 5.0"
+		}
+	}
+}
+
+resource "aws_s3_bucket" "this" {
+	bucket = var.bucket_name
+}
