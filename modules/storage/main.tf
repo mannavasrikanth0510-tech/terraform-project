80091f770b2f92a07919cb98c7905aa17f6a8bc8
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-27 10:21:19 -0500

    terraform

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,14 +1,14 @@
 terraform {
-	required_version = ">= 1.6.0"
+  required_version = ">= 1.6.0"
 
-	required_providers {
-		aws = {
-			source  = "hashicorp/aws"
-			version = "~> 5.0"
-		}
-	}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 resource "aws_s3_bucket" "this" {
-	bucket = var.bucket_name
+  bucket = var.bucket_name
 }
