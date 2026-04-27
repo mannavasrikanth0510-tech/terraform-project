de1a16a97a2c1ffd3715da6e23cd89fd19da4161
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-27 15:00:43 -0500

    terraform

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -12,14 +12,3 @@ terraform {
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_prefix
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
