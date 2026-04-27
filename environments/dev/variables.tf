7f4809b6ce7f66ba4dbd05d425e220a76c4fa2a9
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-04-27 11:20:13 -0500

    terraform

---
+++ environments/dev/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region for the dev environment"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket to create in dev"
+  type        = string
+}
