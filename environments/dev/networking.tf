198795a8dd4702508bdf06dfb0e6774daf6345ef
mannavasrikanth0510-tech <mannavasrikanth0510@gmail.com>
2026-05-06 11:51:27 -0500

    changes

--- environments/dev/networking.tf
+++ environments/dev/networking.tf
@@ -23,18 +23,5 @@ output "internet_gateway_id" {
   value       = module.networking.internet_gateway_id
 }
 
-terraform {
-  required_version = ">= 1.6.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
 
--- environments/dev/storage.tf
+++ environments/dev/storage.tf
@@ -3,20 +3,6 @@ module "storage" {
   bucket_prefix = var.bucket_prefix
 }
 
-terraform {
-  required_version = ">= 1.6.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
 
 output "bucket_name" {
   description = "Name of the dev S3 bucket"
